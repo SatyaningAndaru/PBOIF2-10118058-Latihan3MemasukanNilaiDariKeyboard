a35c28d0a7542f26ab23beb371533c7018f5165c
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 17:51:56 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan3/PBOIF210118058Latihan3.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author user
+ * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Masukan nama anda : ");
+        Scanner scanner = new Scanner(System.in);
+        
+        String nama = scanner.next();
+        System.out.println("Nama anda adalah  " + nama);
+    }
+    
+}
